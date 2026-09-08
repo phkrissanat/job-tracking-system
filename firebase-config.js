5f5ea1b233af21f7c7fe52452ff3ee83cd201444
phkrissanat <phkrissanath@gmail.com>
2026-09-08 15:11:38 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDH9l2tKv_hpmIZiriWFUHgIfRJJL3HZ4E",
+  authDomain: "job-tracking-system-5752c.firebaseapp.com",
+  projectId: "job-tracking-system-5752c",
+  storageBucket: "job-tracking-system-5752c.firebasestorage.app",
+  messagingSenderId: "843558996100",
+  appId: "1:843558996100:web:a5e310713c01ed5ece9ad1"
+};
